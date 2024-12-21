217fc06c8c439b4e82bb8892bc36017a23eab9e5
Znno <ziadtabour205@gmail.com>
2024-12-21 20:15:26 +0200

    smth

--- Job Board System/JBS/src/main/java/org/example/jbs/Main.java
+++ Job Board System/JBS/src/main/java/org/example/jbs/Main.java
@@ -1,27 +1,34 @@
 package org.example.jbs;
 
 import atlantafx.base.theme.PrimerDark;
-import atlantafx.base.theme.PrimerLight;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-//        Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
         primaryStage.setTitle("Main Window");
-
 
         Button loginButton = new Button("Login");
         Button signUpButton = new Button("Sign Up");
 
-        loginButton.setOnAction(e -> new LoginForm().start(new Stage()));
-        signUpButton.setOnAction(e -> new SignUpForm().start(new Stage()));
+        loginButton.setOnAction(e -> {
+            Stage loginStage = new Stage();
+            new LoginForm().start(loginStage);
+            primaryStage.hide();
+            loginStage.setOnCloseRequest(event -> primaryStage.show());
+        });
+
+        signUpButton.setOnAction(e -> {
+            Stage signUpStage = new Stage();
+            new SignUpForm().start(signUpStage);
+            primaryStage.hide();
+            signUpStage.setOnCloseRequest(event -> primaryStage.show());
+        });
 
         VBox layout = new VBox(10);
         layout.getChildren().addAll(loginButton, signUpButton);
@@ -29,7 +36,6 @@ public class Main extends Application {
         Scene scene = new Scene(layout, 300, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
