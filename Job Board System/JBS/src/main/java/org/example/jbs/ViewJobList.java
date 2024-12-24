aaa2836b3f22c3d80268aa9f125d2a612c5b6df0
I-Elhadad <hemaelhadad26@gmail.com>
2024-12-24 23:20:52 +0200

    can't apply twice

--- Job Board System/JBS/src/main/java/org/example/jbs/ViewJobList.java
+++ Job Board System/JBS/src/main/java/org/example/jbs/ViewJobList.java
@@ -3,6 +3,7 @@ package org.example.jbs;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -12,10 +13,7 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class ViewJobList extends Application {
     int user_id;
@@ -59,27 +57,25 @@ public class ViewJobList extends Application {
                 String title = rs.getString("title");
                 String requirements = rs.getString("requirements");
                 String description = rs.getString("description");
-                int job_id=rs.getInt("id");
-                String query1="SELECT id FROM jobseeker_profile WHERE user_id=?";
-                PreparedStatement stmt1=conn.prepareStatement(query1);
-                stmt1.setInt(1,user_id);
-                ResultSet rs1=stmt1.executeQuery();
-                int jobseeker_id=0;
-                if(rs1.next())
-                {
-                    jobseeker_id=rs1.getInt("id");
+                int job_id = rs.getInt("id");
+                String query1 = "SELECT id FROM jobseeker_profile WHERE user_id=?";
+                PreparedStatement stmt1 = conn.prepareStatement(query1);
+                stmt1.setInt(1, user_id);
+                ResultSet rs1 = stmt1.executeQuery();
+                int jobseeker_id = 0;
+                if (rs1.next()) {
+                    jobseeker_id = rs1.getInt("id");
                 }
 
-                String query2="SELECT state FROM applicants_details WHERE jobseeker_id=? AND job_id=?";
+                String query2 = "SELECT state FROM applicants_details WHERE jobseeker_id=? AND job_id=?";
 
-                PreparedStatement stmt2=conn.prepareStatement(query2);
-                stmt2.setInt(1,jobseeker_id);
-                stmt2.setInt(2,job_id);
-                ResultSet rs2=stmt2.executeQuery();
-                String state="Not Applied";
-                if(rs2.next())
-                {
-                    state=rs2.getString("state");
+                PreparedStatement stmt2 = conn.prepareStatement(query2);
+                stmt2.setInt(1, jobseeker_id);
+                stmt2.setInt(2, job_id);
+                ResultSet rs2 = stmt2.executeQuery();
+                String state = "Not Applied";
+                if (rs2.next()) {
+                    state = rs2.getString("state");
                 }
 
                 Label jobLabel = new Label(title);
@@ -87,11 +83,22 @@ public class ViewJobList extends Application {
                 Button viewButton = new Button("View");
 
                 viewButton.setOnAction(e -> {
-                    System.out.println(user_id);
-                    Stage jobDetailStage = new Stage();
-                    new ViewJobDetails(title, description, requirements, employer_id, user_id,job_id).start(jobDetailStage);
-                    jobstage.hide();
-                    jobDetailStage.setOnCloseRequest(event -> jobstage.show());
+                    String temp = "Not Applied";
+                    temp = stateLabel.getText();
+                    if (temp.equals("Not Applied")) {
+                        System.out.println(user_id);
+                        Stage jobDetailStage = new Stage();
+                        new ViewJobDetails(title, description, requirements, employer_id, user_id, job_id).start(jobDetailStage);
+                        jobstage.hide();
+                        jobDetailStage.setOnCloseRequest(event -> jobstage.show());
+                    } else {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Already Applied");
+                        alert.setHeaderText(null);
+                        alert.setContentText("You have already applied to this job.");
+                        alert.showAndWait();
+
+                    }
 
                 });
 
@@ -99,7 +106,7 @@ public class ViewJobList extends Application {
                 Region spacer = new Region();
                 HBox.setHgrow(spacer, Priority.ALWAYS);
 
-                jobEntry.getChildren().addAll(jobLabel,stateLabel, spacer, viewButton);
+                jobEntry.getChildren().addAll(jobLabel, stateLabel, spacer, viewButton);
                 jobListView.getItems().add(jobEntry);
             }
 
